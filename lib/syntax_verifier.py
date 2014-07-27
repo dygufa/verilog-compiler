4833a2e647a207b0995d9386e02d16593a82d275
Rodrigo Araujo <dygufa@gmail.com>
2014-07-27 18:08:52 -0300

    Adicionada camada de verificação de declaraçõ rpevia das variaveis

--- lib/syntax_verifier.py
+++ lib/syntax_verifier.py
@@ -3,6 +3,17 @@ import re
 def init(file_object):
 	return check_syntax(file_object)
 
+def array_in_array(array_1, array_2):
+	not_found_elements = []
+	for el_1 in array_1:
+		if not el_1 in array_2:
+			not_found_elements.append(el_1)
+
+	not_found_elements_length = len(not_found_elements)
+	returned_dict = {'status': not not_found_elements_length}
+	if not_found_elements_length:
+		returned_dict['not_found_elements'] = not_found_elements
+	return returned_dict
 
 def check_syntax(file_object):
 	file_lines = file_object.readlines();
@@ -13,6 +24,12 @@ def check_syntax(file_object):
 	extracted_data = {}
 	k = 0
 	error = False
+	module_args = []
+
+	extracted_data['outputs'] = []
+	extracted_data['inputs'] = []
+	extracted_data['wires'] = []
+	extracted_data['ports'] = []
 
 	for line in file_lines:
 		k += 1
@@ -30,27 +47,31 @@ def check_syntax(file_object):
 
 			res_xput = is_xput(line)
 			if res_xput['status']:
+				res_array_in_array = array_in_array(res_xput['args'], module_args)
+				if not res_array_in_array['status']:
+					error = True
+					print(res_xput['type'].title() + '(s) não declarado(s) no module na linha ' + str(k) + ': ' + ', '.join(res_array_in_array['not_found_elements']))
+					break;
+
 				if res_xput['type'] == 'input':
-					if not 'inputs' in extracted_data:
-						extracted_data['inputs'] = []
 					extracted_data['inputs'] = extracted_data['inputs'] + res_xput['args']
 				elif res_xput['type'] == 'output':
-					if not 'outputs' in extracted_data:
-						extracted_data['outputs'] = []
 					extracted_data['outputs'] = extracted_data['outputs'] + res_xput['args']
 			else:
 				res_port = is_port(line) 
 				if res_port['status']:
-					if not 'ports' in extracted_data:
-						extracted_data['ports'] = []
+					res_array_in_array = array_in_array([res_port['output']] + res_port['inputs'], extracted_data['inputs'] + extracted_data['outputs'] + extracted_data['wires'])
+					if not res_array_in_array['status']:
+						error = True
+						print('Entrada(s) ou saída não encontrado(s) na porta na linha ' + str(k) + ': ' + ', '.join(res_array_in_array['not_found_elements']))
+						break;
+
 					current_port_data = [{'id': res_port['name'], 'type': res_port['type'], 'delay': res_port['delay'], 'output': res_port['output'], 'inputs': res_port['inputs']}]
 					extracted_data['ports'] = extracted_data['ports'] + current_port_data
 				else:
 					res_datatype = is_datatype(line)
 					if res_datatype['status']:
 						if res_datatype['type'] == 'wire':
-							if not 'wires' in extracted_data:
-								extracted_data['wires'] = []
 							extracted_data['wires'] = extracted_data['wires'] + res_datatype['args']
 					else:
 						error = True
@@ -59,11 +80,13 @@ def check_syntax(file_object):
 
 					
 		else:
-			if (is_module(line)):
+			res_module = is_module(line)
+			if (res_module['status']):
+				module_args = res_module['args']
 				module_defined = True
 			else:
 				error = True
-				print('Erro, o código verilog não foi iniciado com module')
+				print('Erro, o código verilog não foi iniciado com module corretamente')
 				break
 
 	if error:
